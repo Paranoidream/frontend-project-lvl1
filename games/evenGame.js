a7957cfa240d9056a06bfb844ad781a4f78576b8
Aleksandra <missing1311@gmail.com>
2022-07-21 17:59:23 +0300

    step5,5

--- games/evenGame.js
+++ games/evenGame.js
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import random from '../src/random.js';
 
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hello ' + userName + '!');
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 export default () => {
 for (let i = 0; i < 3; i += 1) {
-    const number = (min = 0, max = 100) => {
-        const randomNum = Math.floor(Math.random() * (max - min)) + min;
-        return randomNum;
-      }
-      console.log('Question: ' + number());
+      console.log('Question: ' + random());
     let answer = readlineSync.question('Your answer: ');
     let rightAnswer = '';
-    if (number % 2 === 0) {
+    if (random % 2 === 0) {
         rightAnswer = 'yes';
     } else {
         rightAnswer = 'no';
-    };
-    if (answer !== rightAnswer) {
+    }
+    while (answer !== rightAnswer) {
         console.log(`"${answer}" + 'is wrong answer ;(. Correct answer was ' + "${rightAnswer}"`);
         console.log("Let's try again, " + userName + "!");
         return false;
