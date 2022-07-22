c31325b4bfd4b3cb36575d53fd987fdb740df6cb
Aleksandra <missing1311@gmail.com>
2022-07-22 12:30:07 +0300

    fixed welcoming

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
 console.log('Welcome to the Brain-Games!');
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;
