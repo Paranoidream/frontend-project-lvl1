5518625ca4138440a4c7caea16b1e8b68b412838
Aleksandra <missing1311@gmail.com>
2022-07-24 10:57:41 +0300

    random fixed

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -6,7 +6,7 @@ const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const getRoundData = () => {
-  const question = getRandomNumber();
+  const question = getRandomNumber(0, 100);
   const rightAnswer = isEven(question) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
